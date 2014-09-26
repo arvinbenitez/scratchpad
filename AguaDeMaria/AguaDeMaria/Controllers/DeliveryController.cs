68ffaf80e291b898bdb6dccbc112e59b666b5c6c
arvinbenitez <arvin_benitez@yahoo.com>
2014-09-26 22:48:26 +0800

    Initial changes for Delivery

---
+++ AguaDeMaria/AguaDeMaria/Controllers/DeliveryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AguaDeMaria.Models.Delivery;
+
+namespace AguaDeMaria.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        public ActionResult DeliveryEditor(DeliveryParameter parameter)
+        {
+            DeliveryDto dto = null;
+            return PartialView(dto);
+        }
+    }
+}
---
+++ AguaDeMaria/AguaDeMaria/Models/Delivery/DeliveryParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AguaDeMaria.Models.Delivery
+{
+    public class DeliveryParameter
+    {
+        public int? OrderId { get; set; }
+        public int? DeliveryId { get; set; }
+    }
+}
