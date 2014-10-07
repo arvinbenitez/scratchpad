ac8907570c99e295453c2c3ad1e2690282a1140d
arvinbenitez <arvin_benitez@yahoo.com>
2014-10-07 22:34:52 +0800

    Sample WCF code with exception

---
+++ ConsoleWCFService/ConsoleWCFClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWCFClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var proxy = new HelloWorldServiceClient();
+                Console.WriteLine(proxy.SayHello(null));
+            }
+            catch (FaultException fe)
+            {
+                Console.WriteLine("Caught a fault exception");
+                Console.WriteLine(fe);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleWCFService/ConsoleWCFService/HelloWorldService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWCFService
+{
+    [ServiceContract]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        string SayHello(string name);
+
+    }
+
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    class HelloWorldService: IHelloWorldService
+    {
+        public string SayHello(string name)
+        {
+            
+            return "Hello " + name.Length;
+        }
+    }
+}
