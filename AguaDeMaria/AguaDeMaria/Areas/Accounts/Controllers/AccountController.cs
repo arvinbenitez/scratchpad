d8c4a6d315ac447901c8712df12dae138f16c0b1
arvinbenitez <arvin_benitez@yahoo.com>
2014-10-26 10:58:26 +0800

    Added accounts area and a link to invoices

---
+++ AguaDeMaria/AguaDeMaria/Areas/Accounts/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AguaDeMaria.Areas.Accounts.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Accounts/Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
