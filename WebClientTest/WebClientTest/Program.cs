66279efd73f99da5cd1cf2afce83caf07fa41276
arvinbenitez <arvin_benitez@yahoo.com>
2014-12-22 20:59:15 +0800

    WebClient and HttpClient test application

---
+++ WebClientTest/WebClientTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Http;
+
+namespace WebClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:88");
+                var details = new List<KeyValuePair<string, string>>{
+                    new KeyValuePair<string,string>("EmailAddress","admin@rentthatbike.com"),
+                    new KeyValuePair<string,string>("Password","admin"),
+                    new KeyValuePair<string,string>("Redirect","/Home")
+                };
+                var formContent = new FormUrlEncodedContent(details);
+                var response = client.PostAsync(@"/login/login", formContent).Result;
+                var asdjk = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var another = new FormUrlEncodedContent(details);
+                    var xesponse = client.PostAsync(@"/login/testaction", another).Result;
+                    var resultxx = xesponse.Content.ReadAsStringAsync().Result;
+
+                };
+                Console.WriteLine("Done Posting");
+                //var getResponse = client.GetStringAsync("/login/index").Result;
+                //if (getResponse.IsSuccessStatusCode)
+                //{
+                //    Console.WriteLine("Response is: /n{0}", getResponse.ToString());
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Not quite");
+                //}
+                Console.WriteLine("Done with post and get");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ WebPostFormCollectionTest/WebPostFormCollectionTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using ServiceStack;
+using System.Net;
+using System.Net.Http;
+
+
+namespace WebPostFormCollectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new ServiceStack.JsonServiceClient("http://localhost:88");
+
+            var dictionary = new NameValueCollection
+            {
+                {"field1","arvin"},
+                {"field2","benitez"}
+            };
+
+            var loginParameter = new
+            {
+                EmailAddress = "admin@rentthatbike.com",
+                Password = "admin",
+                Redirect = "/Home"
+            };
+
+            //perform login
+            var result = client.Post<string>("/Login/LogIn", loginParameter);
+            string jsonParameter = "{\"FormData\":[{\"Field1\":\"arvin\"}]}";
+            var x = client.Post<string>("/Login/TestAction", jsonParameter);
+
+            var responsex = "http://localhost:88/login/testaction".PostToUrl(dictionary.ToFormUrlEncoded());
+
+            ////string formData = "field1=value1&field2=value2&field3=value3";
+            ////string values = "[{\"field1\" : \"value1\"},{\"field2\" : \"value2\"}]";
+            ////string values = "{Field1: \"value1\", Field2: \"value2\"}";
+
+            ////var result = client.Post<string>("/login/testaction", dictionary.ToFormUrlEncoded());
+            //var result = client.Get<string>("/login/testaction/0?" + dictionary.ToFormUrlEncoded());
+
+        }
+
+        public class CreateNodeDto : NameValueCollection, IReturn<string>
+        {
+
+        }
+    }
+}
