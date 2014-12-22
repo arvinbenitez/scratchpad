c1b7f8a1e834b8222cf78e1e02893f59c4146a6a
arvinbenitez <arvin_benitez@yahoo.com>
2014-12-22 21:03:17 +0800

    angular routing

--- AguaDeMaria/AguaDeMaria/Areas/Accounts/Scripts/app.js
+++ AguaDeMaria/AguaDeMaria/Areas/Accounts/Scripts/app.js
@@ -2,26 +2,25 @@
     "use strict";
 
     var myAppModule = angular.module('myApp', ['ui.router']);
-    myAppModule.config(function ($stateProvider, $urlRouterProvider) {
-        // For any unmatched url, send to /users
-        //$urlRouterProvider.otherwise("/users");
+    //myAppModule.config(function ($stateProvider, $urlRouterProvider) {
+    //    // For any unmatched url, send to /users
+    //    //$urlRouterProvider.otherwise("/users");
 
-        $stateProvider
-          .state('users', {
-              url: "/users",
-              templateUrl: "areas/accounts/scripts/views/users.html",
-              controller: "UsersController"
-          })
-         .state('users.new', {
-             url: "/new",
-             templateUrl: "areas/accounts/scripts/views/useredit.html",
-             controller: "UsersController"
-         })
-        .state('users.edit', {
-            url: "/edit/:userId",
-            templateUrl: "areas/accounts/scripts/views/useredit.html",
-            controller: "UsersController"
-        });
-    });
-    //alert('The angular module was setup')
+    //    $stateProvider
+    //      .state('users', {
+    //          url: "/users",
+    //          templateUrl: "areas/accounts/scripts/views/users.html",
+    //          controller: "UsersController"
+    //      })
+    //     .state('users_new', {
+    //         url: "users/new",
+    //         templateUrl: "areas/accounts/scripts/views/useredit.html",
+    //         controller: "UsersController"
+    //     })
+    //    .state('users_edit', {
+    //        url: "users/edit/:userId",
+    //        templateUrl: "areas/accounts/scripts/views/useredit.html",
+    //        controller: "UsersController"
+    //    });
+    //});
 })();
